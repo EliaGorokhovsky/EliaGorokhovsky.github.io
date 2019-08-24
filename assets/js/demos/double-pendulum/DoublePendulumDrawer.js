cbd0a9569d116820b80e1820edc4969f60aff6dc
EliaGorokhovsky <eliapgorokhovsky@gmail.com>
2019-08-24 15:15:41 -0600

    Add tracing to double pendulum

--- assets/js/demos/double-pendulum/DoublePendulumDrawer.js
+++ assets/js/demos/double-pendulum/DoublePendulumDrawer.js
@@ -16,8 +16,16 @@ class DrawingApp {
         this.context.strokeStyle = "black";
         this.context.lineWidth = 1;
         this.lastRender = 0;
+        this.tracing = true;
+        this.traceLength = 500;
         
-        this.pendulum = new DoublePendulum(0.1, 0.06, 0.7, 1, Math.PI / 2, Math.PI / 6, 2, 1, 9.8, 6)
+        this.pendulum = new DoublePendulum(0.1, 0.06, 0.7, 1, Math.PI / 2, Math.PI / 6, 2, 1, 9.8, 0);
+        let x1 = this.canvas.width / 2 + this.canvas.width / 4 * this.pendulum.length1 * Math.sin(this.pendulum.angle1);
+        let y1 = this.canvas.height / 2 + this.canvas.width / 4 * this.pendulum.length1 * Math.cos(this.pendulum.angle1);
+        let x2 = x1 + this.canvas.width / 4 * this.pendulum.length2 * Math.sin(this.pendulum.angle2);
+        let y2 = y1 + this.canvas.width / 4 * this.pendulum.length2 * Math.cos(this.pendulum.angle2);
+        this.timeseries1 = [{x: x1, y: y1}];
+        this.timeseries2 = [{x: x2, y: y2}];
     }
 
     /**
@@ -41,6 +49,15 @@ class DrawingApp {
         let y1 = this.canvas.height / 2 + this.canvas.width / 4 * this.pendulum.length1 * Math.cos(this.pendulum.angle1);
         let x2 = x1 + this.canvas.width / 4 * this.pendulum.length2 * Math.sin(this.pendulum.angle2);
         let y2 = y1 + this.canvas.width / 4 * this.pendulum.length2 * Math.cos(this.pendulum.angle2);
+        this.timeseries1.push({x: x1, y: y1});
+        this.timeseries2.push({x: x2, y: y2});
+        if (this.timeseries1.length > this.traceLength) {
+            this.timeseries1.splice(0, 1);
+        }
+        if (this.timeseries2.length > this.traceLength) {
+            this.timeseries2.splice(0, 1);
+        }
+        //Draw pendulum
         this.context.beginPath();
         this.context.moveTo(this.canvas.width / 2,this.canvas.height / 2);
         this.context.lineTo(x1, y1);
@@ -52,6 +69,25 @@ class DrawingApp {
         this.context.beginPath();
         this.context.arc(x2, y2, this.canvas.width / 4 * this.pendulum.radius2, 0, 2 * Math.PI);
         this.context.fill();
+        //Draw timeseries
+        if (this.tracing) {
+            let style = this.context.strokeStyle;
+            this.context.strokeStyle = "red";
+            this.context.beginPath();
+            this.context.moveTo(this.timeseries1[0].x, this.timeseries1[0].y);
+            this.timeseries1.forEach(element => {
+                this.context.lineTo(element.x, element.y);
+            });
+            this.context.stroke();
+            this.context.beginPath();
+            this.context.strokeStyle = "blue";
+            this.context.moveTo(this.timeseries2[0].x, this.timeseries2[0].y);
+            this.timeseries2.forEach(element => {
+                this.context.lineTo(element.x, element.y);
+            });
+            this.context.stroke();
+            this.context.strokeStyle = style;
+        }
     }
 
     /**
